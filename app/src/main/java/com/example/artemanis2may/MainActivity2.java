10ef1f378e01b2dfe9c183f589ac33f11ac9fb57
Max Korobkov <goosekms1@gmail.com>
2023-05-04 10:19:40 +0300

    artem 7

--- app/src/main/java/com/example/artemanis2may/MainActivity2.java
+++ app/src/main/java/com/example/artemanis2may/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.artemanis2may;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-    }
+
+    Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+        @Override
+        public void run() {
+            Intent intent=new Intent(MainActivity2.this, MainActivity3.class);
+            startActivity(intent);
+        }
+    },2000);
+}
 }
--- app/src/main/java/com/example/artemanis2may/MainActivity3.java
+++ app/src/main/java/com/example/artemanis2may/MainActivity3.java
@@ -2,7 +2,9 @@ package com.example.artemanis2may;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity3.this, MainActivity4.class);
+                startActivity(intent);
+            }
+        },2000);
     }
 }
