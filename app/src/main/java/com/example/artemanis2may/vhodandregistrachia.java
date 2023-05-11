87ae518099d6cd65c3aec6517f37815d23a0bed9
Max Korobkov <goosekms1@gmail.com>
2023-05-11 09:30:47 +0300

    artem 16

--- app/src/main/java/com/example/artemanis2may/vhodandregistrachia.java
+++ app/src/main/java/com/example/artemanis2may/vhodandregistrachia.java
@@ -2,7 +2,9 @@ package com.example.artemanis2may;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class vhodandregistrachia extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class vhodandregistrachia extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vhodandregistrachia);
+    }
+    public void run(View v) {
+        Intent intent=new Intent(vhodandregistrachia.this, yandex.class);
+        startActivity(intent);
     }
 }
