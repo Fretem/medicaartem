34c78e990f1f7d02d0eb2cab3cc38422fa5e4ff4
Max Korobkov <goosekms1@gmail.com>
2023-05-15 16:29:53 +0300

    artem 20

--- app/src/main/java/com/example/artemanis2may/Parol.java
+++ app/src/main/java/com/example/artemanis2may/Parol.java
@@ -2,7 +2,9 @@ package com.example.artemanis2may;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Parol extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Parol extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parol);
+    }
+    public void run(View v) {
+        Intent intent=new Intent(Parol.this, karta.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/artemanis2may/karta.java
@@ -0,0 +1,14 @@
+package com.example.artemanis2may;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class karta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_karta);
+    }
+}
